d899cf77a0daa5d200d5c97e9342c00d6d887b35
Kaizercode <69699616+Kaizercode@users.noreply.github.com>
2022-04-19 11:34:30 +0100

    configure .env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+import dotenv from "dotenv";
+dotenv.config();
 
 module.exports = {
   solidity: "0.8.4",
-   networks: {
+  networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/SdK6d1bn1DIz38tXW8s9IWbJ90w8l1XW",
-      accounts: [
-        "ae6299370a324e801c2d005d5dcc965cd4e1d29d75832e55bb80f6e2ae8bd8ca"
-      ],
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
